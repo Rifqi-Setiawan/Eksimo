695ce92358d5962d656b6fc984be4212c7feb48d
Rifqi Setiawan <rifqis232@gmail.com>
2025-05-14 07:49:22 +0700

    update jadi maven

---
+++ src/main/java/com/Ecommerce/tubes_PBO/TubesPboApplication.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.tubes_PBO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesPboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesPboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ecommerce/tubes_PBO/TubesPboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.tubes_PBO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubesPboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
