8f3572f3b77e03d3ba03ab156198f2252cb2d988
Rifqi Setiawan <rifqis232@gmail.com>
2025-05-17 20:47:59 +0700

    memperbaiki get product dan delete serta menambahkan response handler untuk get Product

--- src/main/java/com/Ecommerce/tubes_PBO/controller/ProductController.java
+++ src/main/java/com/Ecommerce/tubes_PBO/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.Ecommerce.tubes_PBO.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Ecommerce.tubes_PBO.model.Product;
+import com.Ecommerce.tubes_PBO.response.ResponseHandler;
 import com.Ecommerce.tubes_PBO.service.ProductService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,21 +20,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
-
-
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
     ProductService productService;
-    public ProductController(ProductService productService){
+
+    public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
-    @GetMapping("/{vendorId}")
-    public Product getProduct(@RequestParam Long vendorId) {
-        return productService.getProduct(vendorId);
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getProductById(@PathVariable Long id) {
+        try {
+            Product product = productService.getProduct(id);
+            return ResponseHandler.responseBuilder("Success", HttpStatus.OK, product);
+        } catch (Exception e) {
+            return ResponseHandler.responseBuilder(e.getMessage(), HttpStatus.NOT_FOUND, null);
+        }
     }
 
     @GetMapping("")
@@ -44,18 +49,17 @@ public class ProductController {
         productService.createProduct(product);
         return "Product berhasil ditambahkan";
     }
-    
+
     @PutMapping("")
     public String updateProduct(@RequestBody Product produt) {
         productService.updateProduct(produt);
         return "Product berhasil diupdate";
     }
 
-    @DeleteMapping("/{vendorId}")
-    public String deleteProduct(@PathVariable("vendorId") Long vendorId) {
-        productService.deleteProduct(vendorId);
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
         return "Product berhasil dihapus";
     }
-    
-    
+
 }
